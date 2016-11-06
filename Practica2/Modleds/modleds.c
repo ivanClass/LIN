5b6b26d3447e0251c005dc2b8d17f9b268e2b7a5
ivanClass <ivan.aguilera103@gmail.com>
2016-11-06 11:08:30 +0100

    Parte B iniciada

--- Practica2/Modleds/modleds.c
+++ Practica2/Modleds/modleds.c
@@ -24,11 +24,59 @@ static inline int set_leds(struct tty_driver* handler, unsigned int mask){
     return (handler->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,mask);
 }
 
+static ssize_t mywrite(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+
+
+	//Coger mensaje buf
+	//Copiarlo a kbuff
+	//
+
+	char *kbuff; //CAMBIAR A NUMEROOOO?¿?¿? COPIAR DE SO PR4
+
+
+	kbuff = (char *)vmalloc(len);
+
+	if ((*off) > 0){ 
+		vfree(kbuff);/* The application can write in this entry just once !! */
+		return 0;
+	}
+
+	/* Transfer data from user to kernel space */
+	if (copy_from_user(&kbuff[0], buf, len )){
+		vfree(kbuff);
+		return -EFAULT;
+	}
+
+	kbuff[len] = '\0';
+
+	//LLAMAR A setLeds con el numero de kbuff
+	kbd_driver= get_kbd_driver_handler();
+	set_leds(kbd_driver,ALL_LEDS_ON); 
+
+	//FINAL COPIADO
+	*off+=len;            /* Update the file pointer */
+	vfree(kbuff);
+
+	return len;
+}
+
 static int __init modleds_init(void)
 {	
-   kbd_driver= get_kbd_driver_handler();
-   set_leds(kbd_driver,ALL_LEDS_ON); 
-   return 0;
+	proc_entry = proc_create( "modleds", 0666, NULL, &fops);
+
+	if (proc_entry == NULL) {
+		printk(KERN_INFO "Modleds: Can't create /proc entry\n");
+
+		ret = -ENOMEM;
+	}
+	else {
+		printk(KERN_INFO "Modleds: Module loaded\n");
+	}
+
+	kbd_driver= get_kbd_driver_handler();
+	set_leds(kbd_driver,ALL_LEDS_ON); 
+
+	return 0;
 }
 
 static void __exit modleds_exit(void){
@@ -37,6 +85,10 @@ static void __exit modleds_exit(void){
 
 module_init(modleds_init);
 module_exit(modleds_exit);
+
+static const struct file_operations fops = {
+	.write = mywrite
+};
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Modleds");
--- Practica2/cpuinfo.c
+++ Practica2/cpuinfo.c
@@ -12,18 +12,6 @@
 #define SYS_read          0 
 #endif
 
-#ifndef SYS_open 
-#define SYS_open          2 
-#endif
-
-#ifndef SYS_write 
-#define SYS_write         1 
-#endif
-
-#ifndef SYS_close 
-#define SYS_close         3 
-#endif
-
 
 int main(void)
 {
@@ -32,8 +20,7 @@ int main(void)
     char buf[BUFSIZE+1];
     
     /* Open /proc entry in read-only mode */
-    //fd=open("/proc/cpuinfo",O_RDONLY);
-    fd = syscall(SYS_open, "/proc/cpuinfo",O_RDONLY);
+    fd=open("/proc/cpuinfo",O_RDONLY);
     
     if (fd<0){
         fprintf(stderr,"Can't open the file\n");
@@ -43,8 +30,7 @@ int main(void)
     /* Loop that reads data from the file and prints its contents to stdout */
     while((read_chars=syscall(SYS_read,fd,buf,BUFSIZE))>0){
         buf[read_chars]='\0';
-        //printf("%s",buf); 
-        syscall(SYS_write, 1, buf, BUFSIZE);           
+        printf("%s",buf);            
     }
      
     if (read_chars<0){
@@ -54,7 +40,6 @@ int main(void)
     
     	
     /* Close the file and exit */ 
-    //close(fd);
-    syscall(SYS_close, fd);
+    close(fd);
     return 0;
 }
