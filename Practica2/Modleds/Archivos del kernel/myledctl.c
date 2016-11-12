9f9bd71c13061988533af95c6b0f95f124172c44
ivanClass <ivan.aguilera103@gmail.com>
2016-11-12 20:46:28 +0100

    -Falta control cadena de entrada (echo uu). -En blinkdrvc implementar [][] para los leds.

---
+++ Practica2/Modleds/Archivos del kernel/myledctl.c
@@ -0,0 +1,43 @@
+								#include <linux/syscalls.h>
+#include <linux/kernel.h>
+#include <linux/module.h> 
+#include <asm-generic/errno.h>
+#include <linux/init.h>
+#include <linux/tty.h>      /* For fg_console */
+#include <linux/kd.h>
+#include <linux/proc_fs.h>       /* For KDSETLED */
+#include <linux/vt_kern.h>
+
+struct tty_driver* kbd_driver= NULL;
+
+/* Get driver handler */
+struct tty_driver* get_kbd_driver_handler(void){
+   printk(KERN_INFO "modleds: loading\n");
+   printk(KERN_INFO "modleds: fgconsole is %x\n", fg_console);
+   return vc_cons[fg_console].d->port.tty->driver;
+}
+
+/* Set led state to that specified by mask */
+static inline int set_leds(struct tty_driver* handler, unsigned int mask){
+    return (handler->ops->ioctl) (vc_cons[fg_console].d->port.tty, KDSETLED,mask);
+}
+
+SYSCALL_DEFINE1(ledctl, unsigned int, leds){
+	//printk(KERN_DEBUG "Hello world!\n");
+	int ret = 0;
+	//CUERPO DE LA FUNCIÓN
+	int a1 = leds & 0x4;
+	int a2 = leds & 0x2;
+	int a3 = leds & 0x1;
+
+	a1 = a1 >> 1;
+	a2 = a2 << 1;
+	
+	leds = a1 + a2 + a3;
+	
+	kbd_driver= get_kbd_driver_handler();
+
+	ret = set_leds(kbd_driver, leds); 
+
+	return ret;
+}
---
+++ Practica2/Modleds/ledctl_invoke.c
@@ -0,0 +1,31 @@
+#include <linux/errno.h>
+#include <sys/syscall.h>
+#include <linux/unistd.h>
+#include <stdio.h>
+#ifdef __i386__
+#define __NR_LEDCTL    353
+#else
+#define __NR_LEDCTL    316
+#endif
+
+long ledctl(unsigned int leds){
+	return (long) syscall(__NR_LEDCTL, leds);
+}
+
+int main(int argc, char *argv[]){
+	unsigned int i;
+	int ret = 0;
+
+	if(argc >= 2)
+		sscanf(argv[1],"%x", &i);
+	else
+		i = 0;
+
+	ret = ledctl(i);
+
+	//GUARDAR VALOR RETORNO DE ledctl
+	if(ret == -1)
+		perror("Error!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+	return ret;
+}
